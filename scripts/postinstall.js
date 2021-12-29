10399fa24e42ee6dadc72acfff1da778ce283d2f
vilien <bin_jly@163.com>
2021-12-29 13:15:55 +0800

    modify postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,6 @@
 const https = require('https');
+const { tmpdir } = require('os');
+const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
@@ -12,35 +14,47 @@ const download = async (url, file = 'download.tmp') => new Promise((resolve, rej
   }).on('error', reject);
 });
 
+const rm = async (dir) => new Promise((resolve, reject) => {
+  if (dir && dir !== '/') {
+    execSync(`rm -rf ${dir}`);
+    resolve(true);
+  } else {
+    reject(new Error(`Illegal directory: '${dir}'`));
+  }
+});
+
+
 const unzip = async (filename, dir = './') => new Promise((resolve) => {
   console.log('unzip', filename);
-  execSync(`unzip -o -q -d ${dir} ${filename}`);
+  execSync(`tar -C ${dir} -xzf ${filename}`);
   resolve(true);
 });
 
 const cp = async (src, dist) => new Promise((resolve) => {
   console.log('cp', src, '==>', dist);
-  execSync(`rm -rf ${dist}`);
+  rm(dist);
   execSync(`cp -rf ${src} ${dist}`);
   resolve(true);
-})
+});
 
 const main = async () => {
   console.log('Replacing modules...');
-  const filename = await download('https://codeload.github.com/vilien/strapi-lite/zip/refs/heads/master');
+  const filename = await download('https://codeload.github.com/vilien/strapi-lite/tar.gz/refs/tags/v0.0.1');
+  const basename = path.basename(filename, '.tar.gz');
+  const tmpDir = fs.mkdtempSync(path.join(tmpdir(), 'strapi-'));
   try {
-    await unzip(filename, 'tmp');
-    await cp('./tmp/strapi-lite-master/packages/strapi', 'node_modules/strapi');
-    await cp('./tmp/strapi-lite-master/packages/strapi-database', 'node_modules/strapi-database');
-    await cp('./tmp/strapi-lite-master/packages/strapi-utils', 'node_modules/strapi-utils');
-    await cp('./tmp/strapi-lite-master/packages/strapi-connector-mongoose', 'node_modules/strapi-connector-mongoose');
+    await unzip(filename, tmpDir);
+    await cp(`${tmpDir}/${basename}/packages/strapi`, 'node_modules/strapi');
+    await cp(`${tmpDir}/${basename}/packages/strapi-database`, 'node_modules/strapi-database');
+    await cp(`${tmpDir}/${basename}/packages/strapi-utils`, 'node_modules/strapi-utils');
+    await cp(`${tmpDir}/${basename}/packages/strapi-connector-mongoose`, 'node_modules/strapi-connector-mongoose');
   } catch(err) {
     console.error(err);
   }
 
   // 清理文件
   fs.unlinkSync(filename);
-  execSync('rm -rf ./tmp');
+  rm(tmpDir);
 };
 
 main();
